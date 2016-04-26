ef13e1a6463decfac8416f1dfc397fc8aa1c155c
Lunar <Lunar@lunardemacbook-pro.local>
2016-04-26 18:51:21 +0800

    Initial Commit

---
+++ LunarTagLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LunarTagLabel
+//
+//  Created by Lunar on 16/4/26.
+//  Copyright © 2016年 Lunar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LunarTagLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LunarTagLabel
+//
+//  Created by Lunar on 16/4/26.
+//  Copyright © 2016年 Lunar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
